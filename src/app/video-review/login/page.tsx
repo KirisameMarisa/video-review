d6e9458a407a565da05c3e71ecfeb8dd18986c6b
AritaYuto <marisa.love0925@gmail.com>
2025-12-16 08:18:05 +0900

    Added the settings page to the login page

--- src/app/video-review/login/page.tsx
+++ src/app/video-review/login/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import Login from "@/components/login";
+import { SettingPopover } from "@/components/setting";
 
 export default function LoginPage() {
     return (
         <div className="flex h-screen">
             <Login />
+            <SettingPopover />
         </div>
     );
 }
