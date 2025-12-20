c9ba3ff10d6e673d81486962b44cee184a17dd31
AritaYuto <marisa.love0925@gmail.com>
2025-12-20 16:32:06 +0900

    add demo compose

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -6,8 +6,9 @@ until pg_isready -h db -p 5432; do
   sleep 1
 done
 
-echo "Database is ready"
-npx prisma migrate deploy
-npx prisma generate
+echo "Database setup"
+npm run prisma:deploy
+npm run prisma:generate
 
+echo "Service start"
 npm run dev
